4b8df97fc0abc9335e8d8c7e2fa2c05da697796e
hellooying <maocaoqiu@gmail.com>
2019-05-19 08:53:12 +0800

    清除学习前置部分

--- itcast-service-provider/src/main/java/cn/itcast/service/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.service.mapper;
-
-import cn.itcast.service.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends tk.mybatis.mapper.common.Mapper<User>{
-}
